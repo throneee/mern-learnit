ceffcf94a628615a0239a9776857cf2c6d6dc779
throneee <89823663+luongcao2202@users.noreply.github.com>
2022-02-15 11:06:03 +0700

    Prepare Deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
-        : 'someUrlDeploy';
+        : 'https://hidden-fjord-03232.herokuapp.com/api';
 export const LOCAL_STORAGE_TOKEN_NAME = 'mern-learnit';
 
 export const ADD_POST = 'ADD_POST';
